b4d056b15620530ca4bc141ef3ccc37182acbb2b
Abdelbassite Badou <a.badou@sanadtech.com>
2022-09-23 17:41:12 +0100

    Wrote a color swatch selector groupe (For the color filter)

--- components/moleculs/FilterBlocks.tsx
+++ components/moleculs/FilterBlocks.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from "react";
-import { Checkbox } from "@mantine/core";
+import { Checkbox, CheckIcon, ColorSwatch, Group } from "@mantine/core";
 
 interface CheckboxGroupProps {
   options: {
@@ -32,5 +32,41 @@ export const CheckboxGroup: FunctionComponent<CheckboxGroupProps> = ({
         />
       ))}
     </Checkbox.Group>
+  );
+};
+
+interface ColorSwatchSelectionGroupProps {
+  colors: string[];
+  onChange(value: string[]): void;
+}
+
+export const ColorSwatchSelectionGroup: FunctionComponent<
+  ColorSwatchSelectionGroupProps
+> = ({ colors, onChange }) => {
+  const [selectedColors, setSelectedColors] = React.useState<string[]>(colors);
+  const onCheck = (color: string) => {
+    let newSelectedColors = [];
+    if (selectedColors.includes(color)) {
+      newSelectedColors = selectedColors.filter((c) => c !== color);
+    } else {
+      newSelectedColors = [...selectedColors, color];
+    }
+    setSelectedColors(newSelectedColors);
+    onChange(newSelectedColors);
+  };
+  return (
+    <Group position="center" spacing="xs">
+      {colors.map((color) => (
+        <ColorSwatch
+          key="color"
+          component="button"
+          color={color}
+          onClick={() => onCheck(color)}
+          sx={{ color: "#fff", cursor: "pointer" }}
+        >
+          {selectedColors.includes(color) && <CheckIcon width={10} />}
+        </ColorSwatch>
+      ))}
+    </Group>
   );
 };
--- components/organisms/FiltersPanel.tsx
+++ components/organisms/FiltersPanel.tsx
@@ -1,66 +1,71 @@
-import React, { FunctionComponent } from 'react';
-import { Accordion, Text } from "@mantine/core";
-import { CheckboxGroup } from '@components/moleculs';
+import React, { FunctionComponent } from "react";
+import { Accordion, Text, useMantineTheme } from "@mantine/core";
+import { CheckboxGroup, ColorSwatchSelectionGroup } from "@components/moleculs";
 
-interface FiltersPanelProps {
-    
-}
- 
+interface FiltersPanelProps {}
+
 const FiltersPanel: FunctionComponent<FiltersPanelProps> = () => {
-    return (
-      <div>
-        <Text
-          size="lg"
-          weight="bolder"
-          style={{
-            padding: "1rem",
-          }}
-        >
-          Filters
-        </Text>
-        <Accordion radius="md" defaultValue="Model">
-          <Accordion.Item value="Model">
-            <Accordion.Control>
-              <Text size="sm">Model</Text>
-            </Accordion.Control>
-            <Accordion.Panel>
-              <CheckboxGroup
-                options={[
-                  {
-                    id: "Model 1",
-                    label: "Model 1",
-                  },
-                  {
-                    id: "Model 2",
-                    label: "Model 2",
-                  },
-                ]}
-              />
-            </Accordion.Panel>
-          </Accordion.Item>
+  const theme = useMantineTheme();
+  const colors = Object.keys(theme.colors);
+  return (
+    <div>
+      <Text
+        size="lg"
+        weight="bolder"
+        style={{
+          padding: "1rem",
+        }}
+      >
+        Filters
+      </Text>
+      <Accordion radius="md" defaultValue="Model">
+        <Accordion.Item value="Model">
+          <Accordion.Control>
+            <Text size="sm">Model</Text>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <CheckboxGroup
+              options={[
+                {
+                  id: "Model 1",
+                  label: "Model 1",
+                },
+                {
+                  id: "Model 2",
+                  label: "Model 2",
+                },
+              ]}
+            />
+          </Accordion.Panel>
+        </Accordion.Item>
 
-          <Accordion.Item value="Color">
-            <Accordion.Control>
-              <Text size="sm">Color</Text>
-            </Accordion.Control>
-            <Accordion.Panel></Accordion.Panel>
-          </Accordion.Item>
+        <Accordion.Item value="Color">
+          <Accordion.Control>
+            <Text size="sm">Color</Text>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <ColorSwatchSelectionGroup
+              colors={colors}
+              onChange={(colors) => {}}
+            />
+          </Accordion.Panel>
+        </Accordion.Item>
 
-          <Accordion.Item value="Location">
-            <Accordion.Control>
-              <Text size="sm">Location</Text>
-            </Accordion.Control>
-            <Accordion.Panel></Accordion.Panel>
-          </Accordion.Item>
-          <Accordion.Item value="Rating">
-            <Accordion.Control>
-              <Text size="sm">Rating</Text>
-            </Accordion.Control>
-            <Accordion.Panel></Accordion.Panel>
-          </Accordion.Item>
-        </Accordion>
-      </div>
-    );
-}
- 
+        <Accordion.Item value="Location">
+          <Accordion.Control>
+            <Text size="sm">Location</Text>
+          </Accordion.Control>
+          <Accordion.Panel></Accordion.Panel>
+        </Accordion.Item>
+        <Accordion.Item value="Rating">
+          <Accordion.Control>
+            <Text size="sm">Rating</Text>
+          </Accordion.Control>
+          <Accordion.Panel></Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
+};
+
 export default FiltersPanel;
