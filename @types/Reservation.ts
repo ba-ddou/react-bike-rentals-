dab521b74c426466082facbbe5f2905ab514954e
Abdelbassite Badou <a.badou@sanadtech.com>
2022-09-24 21:42:42 +0100

    Init the Reservations interface/derived types

---
+++ @types/Reservation.ts
@@ -0,0 +1,30 @@
+import { Bike } from "./Bike";
+import { User } from "./User";
+
+export interface Reservation {
+  id: string;
+  start: Date;
+  end: Date;
+  user: string;
+  bike: string;
+  bikeSnapshot: {
+    price: number;
+    location: string;
+  };
+  rating?: number;
+  status: ReservationStatus;
+}
+
+export enum ReservationStatus {
+  PENDING,
+  IN_PROGRESS,
+  COMPLETED,
+  CANCELLED,
+}
+
+export type ReservationCreate = Omit<Reservation, "id" | "rating" | "status">;
+
+export type ReservationWithProjections = Omit<Reservation, "user" | "bike"> & {
+  user: User;
+  bike: Bike;
+};
